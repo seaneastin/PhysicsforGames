ca606f491c39cd9eb8c67ab411f602b53c6b5723
Sean Eastin <sean.eastin@gamil.com>
2020-09-10 14:16:58 -0500

    added project files

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallBehavior : MonoBehaviour
+{
+    public float forceOnFire = 300;
+
+    bool fire = false;
+    bool canFire = true;
+
+    Rigidbody rigidbody = null;
+
+    // use this for initialization
+
+    private void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.isKinematic = true;
+    }
+
+    private void Update()
+    {
+        if(Input.anyKeyDown && canFire)
+        {
+            rigidbody.isKinematic = false;
+            rigidbody.AddForce(transform.forward * forceOnFire);
+            canFire = false;
+        }
+    }
+}
